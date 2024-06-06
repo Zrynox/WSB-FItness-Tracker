a2536284954e18498acd75d68b117544423d53f2
Zrynox <129966314+Zrynox@users.noreply.github.com>
2024-06-06 16:23:59 +0200

    Delete WSB-FitnessTracker-Rep directory

--- WSB-FitnessTracker-Rep/src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingProvider.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.wsb.fitnesstracker.training.internal;
-
-import com.capgemini.wsb.fitnesstracker.training.api.Training;
-
-import java.util.List;
-
-public interface TrainingProvider {
-    List<Training> findTrainingsByUserId(Long userId);
-}
